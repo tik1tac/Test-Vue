82afa58f9997f2ad2750de2d1b29c4330afa757b
YURI IVANOV <Yurass228@yandex.ru>
2023-10-25 17:48:29 +0300

    packets

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./pages/**/*.{html,js}", "./components/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
